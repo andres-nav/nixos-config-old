6b6ec1420db93fcbadab3fac42c26f499c6db68b
contact@andresnav.com <Andres Navarro>
2023-03-23 21:14:42 +0100

    (dev) added ssh agent

--- hosts/virtualbox/config.nix
+++ hosts/virtualbox/config.nix
@@ -32,6 +32,7 @@
     neovim 
     git
     tmux
+    firefox
     lazygit
     htop
     kitty
@@ -40,14 +41,14 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
 }
 
